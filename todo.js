ababfbf1896dad16c0a9d4bf0807c12a608b6a5c
sladamo <s-adamo@kellogg.northwestern.edu>
2016-02-07 14:48:22 -0600

    comments

--- todo.js
+++ todo.js
@@ -23,7 +23,7 @@ if (numComments == 1) {
   $(".total").html(numComments + " comment");
 }
 else {
-  $(".total").html(numComments + " comment");
+  $(".total").html(numComments + " comments");
 }
 }
 
