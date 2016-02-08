14f5716389f96672d36a691bdca3b59891bb5a3a
sladamo <s-adamo@kellogg.northwestern.edu>
2016-02-07 22:22:00 -0600

    caribbean video width

--- todo.js
+++ todo.js
@@ -15,7 +15,7 @@ function addItem() {
   var delete_link = '<a href="#" class="link-delete">(Delete)</a>'
   $("ol").append("<li>" + text + " " + delete_link + "</li>");
 
-  var numItems = $("li").length;
+  var numItems = $("li").length-#;
   $(".total").html(numItems + " comments");
 
   // Challenge: Differentiate between 1 item vs. more items
