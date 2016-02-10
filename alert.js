622d40f943bc7e8d946fe541b667193945725c39
sladamo <s-adamo@kellogg.northwestern.edu>
2016-02-09 20:07:53 -0600

    alert 2

--- alert.js
+++ alert.js
@@ -1,1 +1,1 @@
-window.alert("Thanks for visiting my blog! Check back occasionally for exciting updates.");
+window.alert("Hi my name is Stephanie! Thanks for visiting my travel blog. Check back occasionally for exciting updates.");
